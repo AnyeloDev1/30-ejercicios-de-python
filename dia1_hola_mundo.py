186e5181644aa22a5a8e1e48795303a4a1027570
unknown <andradeyordy1118@gmail.com>
2026-01-06 16:47:22 -0500

    Ejercicio #1

---
+++ dia1_hola_mundo.py
@@ -0,0 +1,8 @@
+#Día 1: Hola Mundo y entrada de usuario (pide nombre/edad e imprímelo).
+
+print("Hola mundo!")
+
+nombre = input("Dime tu nombre: ")
+edad = input("Dime tu edad: ")
+
+print("Hola " + nombre + " tu edad es " + edad + " años")
